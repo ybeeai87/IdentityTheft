236469fb93fc8584354ff5078d8ba1a0455fdd28
Yousif <ybeeai@gmail.com>
2021-09-27 13:36:08 -0400

    first commit

---
+++ Day1Exercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Day1Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, welcome to the Identity Stealer!");
+
+            //Exercise: Find out name, age, hometown. Display the info back to them.
+            Console.WriteLine("May I have your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("May I have your age?");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is your hometown?");
+            string homeTown = Console.ReadLine();
+
+            // using string interpolation
+            Console.WriteLine("Bahaha, your identity has been stolen! " +
+                $"My name is now {name}, I am {age} years old, and I am from {homeTown}!!!");
+        }
+    }
+}
